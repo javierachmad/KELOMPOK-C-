f53e5c0bb1453954fe7eafb70f8aa640c14981ef
javierachmad <adekbudih9@gmail.com>
2026-03-07 11:39:28 +0700

    Add files via upload

---
+++ Burung.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace nyoba
+{
+    internal class Burung
+    {
+        // Properti (tanpa get; set;)
+        public string Nama;
+        public double PanjangSayap;
+        public bool BisaTerbang;
+
+        // Konstruktor
+        public Burung(string nama, double panjangSayap, bool bisaTerbang)
+        {
+            Nama = nama;
+            PanjangSayap = panjangSayap;
+            BisaTerbang = bisaTerbang;
+        }
+
+        // Method
+        public void Terbang()
+        {
+            Console.WriteLine(BisaTerbang
+                ? $"{Nama} terbang dengan sayap {PanjangSayap} cm."
+                : $"{Nama} tidak bisa terbang.");
+        }
+
+        public void Berkicau()
+        {
+            Console.WriteLine($"{Nama} berkicau: Cuit cuit!");
+        }
+    }
+}
---
+++ Ikan.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nyoba
+{
+    internal class Ikan
+    {
+    }
+}
