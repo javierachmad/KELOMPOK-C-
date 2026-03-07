ab202cad57f601626e41749f177f86d3fb846467
javierachmad <adekbudih9@gmail.com>
2026-03-07 11:41:11 +0700

    Add files via upload

---
+++ Ikan.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas2
+{
+    class Ikan
+    {
+        // Properti
+        public string Nama;
+        public string Warna;
+        public int PanjangSirip;
+        public bool SedangBerenang;
+
+        // Konstruktor
+        public Ikan(string nama, string warna, int panjangSirip)
+        {
+            Nama = nama;
+            Warna = warna;
+            PanjangSirip = panjangSirip;
+            SedangBerenang = false;
+        }
+
+        // Method berenang menggunakan sirip
+        public void Berenang()
+        {
+            SedangBerenang = true;
+            Console.WriteLine($"{Nama} sedang berenang menggunakan sirip sepanjang {PanjangSirip} cm!");
+        }
+
+        // Method berhenti berenang
+        public void Berhenti()
+        {
+            if (SedangBerenang)
+            {
+                SedangBerenang = false;
+                Console.WriteLine($"{Nama} berhenti berenang.");
+            }
+            else
+            {
+                Console.WriteLine($"{Nama} memang sedang tidak berenang.");
+            }
+        }
+
+        // Method menampilkan info ikan
+        public void TampilkanInfo()
+        {
+            Console.WriteLine($"Nama: {Nama}, Warna: {Warna}, Panjang Sirip: {PanjangSirip} cm");
+            Console.WriteLine($"Sedang Berenang: {SedangBerenang}");
+        }
+    }
+}
